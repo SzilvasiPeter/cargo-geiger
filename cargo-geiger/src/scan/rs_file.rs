e9d7c365704aa9c57d980ee3db4597446e07ce07
SzilvasiPeter <peti.szilvasi95@gmail.com>
2026-09-17 14:23:57 +0200

    fix: filter out .md files from the dependency files

--- cargo-geiger/src/scan/rs_file.rs
+++ cargo-geiger/src/scan/rs_file.rs
@@ -223,6 +223,7 @@ fn add_dir_entries_to_path_buf_hash_set(
         let canonical_paths = dependencies
             .into_iter()
             .flat_map(|(_, dependency_files)| dependency_files)
+            .filter(|file| !file.ends_with(".md"))
             .map(PathBuf::from)
             .map(|pb| workspace_root.join(pb))
             .map(|pb| pb.canonicalize().map_err(|e| RsResolveError::Io(e, pb)));
